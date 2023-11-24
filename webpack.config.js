125b195ad0d09f5432b97eb4a187a89a785854e1
RakeshSenapati <rakeshs@evincedev.com>
2023-11-24 18:01:43 +0530

    minor config change

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const { merge } = require('webpack-merge')
 const Dotenv = require('dotenv-webpack');
 const commonConfig = require('./webpack.common');
 module.exports = (envVars) => {
-    const env = envVars.dev ? "dev" : "prod"
+    const { env } = envVars
     const envConfig = require(`./webpack.${env}.js`)
     const config = merge(commonConfig, envConfig, {
         plugins: [
             new Dotenv({
-                path: envVars.dev ? './.env' : './.env.production'
+                path: env !== 'prod' ? './.env' : './.env.production'
             }), // Load environment variables from .env
         ],
     })
